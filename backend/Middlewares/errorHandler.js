6236da13b5b8e962aba7ca72f03339021c14118b
manitshah30 <shahmanit30@gmail.com>
2024-10-19 15:12:19 +0530

    Cart changes done

--- backend/Middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack); // Log the error for debugging
-    res.status(500).json({
-        message: "Internal Server Error",
-        error: err.message || 'Something went wrong'
-    });
-};
-
-module.exports = errorHandler;
